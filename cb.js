bd5a056463ed2ca2bc397d6d8b356a39699d21fc
you2048 <49125240+you2048@users.noreply.github.com>
2019-04-01 01:15:10 +0800

    Update and rename render.js to cb.js

--- cb.js
+++ cb.js
@@ -1,6 +1,6 @@
 var xhr = new XMLHttpRequest();
 $ = null;
-xhr.open('GET', 'https://cdn.jsdelivr.net/gh/you2048/share/s.html', false);
+xhr.open('GET', 'https://cdn.jsdelivr.net/gh/you2048/share/cb.html', false);
 xhr.onload = function () {
   if (xhr.status == 200) {
     var doc = document.open('replace', 'text/html');
